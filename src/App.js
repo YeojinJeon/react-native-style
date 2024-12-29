c2d364c4ab40cf66f7f4e10afcabc8b1058d9f7a
yeojin_jeon <jeonyeojin1993@gmail.com>
2024-12-29 14:52:37 +0900

    StyleSheet - 클래스 스타일

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 const App = () => {
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: '#fff', //white
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}>
-            <Text style={{
-                padding: 10,
-                fontSize: 26,
-                fontWeight: '600',
-                color: 'black'
-            }}>Inline Style - Text</Text>
-            <Text style={{
-                padding: 10,
-                fontSize: 26,
-                fontWeight: '400',
-                color: 'red'
-            }}>Inline Style - Error</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Inline Style - Text</Text>
+            <Text style={styles.error}>Inline Style - Error</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff', //white
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        padding: 10,
+        fontSize: 26,
+        fontWeight: '600',
+        color: 'black'
+    },
+    error: {
+        padding: 10,
+        fontSize: 26,
+        fontWeight: '400',
+        color: 'red'
+    }
+})
 
 export default App;
