185d1a23a4681d51eb2a3bafcb72d99e2a050624
yeojin_jeon <jeonyeojin1993@gmail.com>
2024-12-29 21:17:02 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import { StyleSheet, View, Text } from "react-native";
 import { viewStyles, textStyles } from "./styles";
 import { Header, Contents, Footer } from "./components/Layout";
 import ShadowBox from "./components/ShadowBox";
+import styled from "styled-components/native";
+import Button from "./components/Button";
+
 const App = () => {
     return (
-        <View style={viewStyles.container}>
-            {/* <Header />
-            <Contents />
-            <Footer /> */}
-            <ShadowBox />
-        </View>
+        <Container>
+            <Button title="Sesac" />
+            <Button title="React Native" />
+        </Container>
     );
 };
+
+const Container = styled.View`
+flex:1;
+background-color: #ffffff;
+align-items: center;
+justify-content: center;
+`;
 
 const classStyles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff', //white
+        backgroundColor: 'yellow',
         alignItems: 'center',
         justifyContent: 'center',
     },
